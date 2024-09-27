f4abd567246a941b6d936e3124838e9a7be7c14d
Oleksii Kovalenko <oleksii_kovalenko@mail.com>
2024-09-27 15:50:13 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirstComponentComponent } from "./first-component/first-component.component";
+import { DirectNameDirective } from './direct-name.directive';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FirstComponentComponent, DirectNameDirective, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  public isHide = false;
+
   title = 'app-001';
+  arr = [1, 2, 3, 4]
 }
